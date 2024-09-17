43ec2822a05b6bd8d45d5c7787f66322a65c9035
blazinh <hadibaalbaki@protom.me>
2024-09-17 13:48:45 +0300

    first commit

---
+++ lua/plugins/color-scheme.lua
@@ -0,0 +1,19 @@
+return {
+  -- nightfox theme
+  {
+    "EdenEast/nightfox.nvim",
+    opts = {
+      options = {
+        transparent = true,
+      },
+    },
+  },
+
+  -- Configure LazyVim to load the theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "duskfox",
+    },
+  },
+}
